123e9512677645347b1ec552d606cb571db06e9f
csteinberg <csteinberg@geekhive.com>
2018-11-30 15:01:06 -0500

    Fixed api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(PORT, () => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // TODO: Change to deployed recipe site
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
