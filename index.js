58a5e29e061bda70f829f7493f3086cde7d49f54
csteinberg <csteinberg@geekhive.com>
2018-11-30 14:51:47 -0500

    Working on it

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -18,8 +17,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-app.use(bodyParser);
 
 app.get('/*', (req, res, next) => {
   const url = req.query.url;
