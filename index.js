b5db93b29080ae88cd0ddaed2f216c8b00026b5c
csteinberg <csteinberg@geekhive.com>
2018-12-03 14:37:47 -0500

    Changed url to prod

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.listen(PORT, () => {
 });
 
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "https://recipemine-prod.firebaseapp.com");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://recipemine-prod.firebaseapp.com");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
